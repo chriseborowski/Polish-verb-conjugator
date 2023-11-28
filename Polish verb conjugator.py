cfc0912f2420971654579a5c6839d6b9494f5bfa
Chris Borowski <krzysztofeborowski@gmail.com>
2023-11-27 23:08:11 -0600

    Add support for past-tense forms of the nosić verb category

--- Polish verb conjugator.py
+++ Polish verb conjugator.py
@@ -919,8 +919,14 @@ nosic_suffix = ["zę", "isz", "i", "imy", "icie", "zą"]
 
 
 def nosic(word):
-    stem = word[:-2]
+    print("Present tense:\n")
     for pronoun, suffix in enumerate(nosic_suffix):
+        stem = word[:-2]
+        conjugated_form = stem + suffix
+        print(pronouns[pronoun] + " " + conjugated_form)
+    print("\nPast tense (masc/fem/neut):\n")
+    for pronoun, suffix in enumerate(robic_past_suffix):
+        stem = word[:-1]
         conjugated_form = stem + suffix
         print(pronouns[pronoun] + " " + conjugated_form)
 
