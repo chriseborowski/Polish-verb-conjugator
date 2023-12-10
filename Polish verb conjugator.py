8d05735c8b6ffaa141b44bfa798ae7022f9f8a29
Chris Borowski <krzysztofeborowski@gmail.com>
2023-12-10 16:32:38 -0600

    Add support for past-tense forms of the prząść exception

--- Polish verb conjugator.py
+++ Polish verb conjugator.py
@@ -1123,11 +1123,24 @@ def bede(word):
 
 # prząść exception
 przasc_suffix = ["ędę", "ędziesz", "ędzie", "ędziemy", "ędziecie", "ędą"]
+przasc_past_suffix = [
+    "ądłem/-ędłam",
+    "ądłeś/-ędłaś",
+    "ądł/-ędła/-ędło",
+    "ędliśmy/-łyśmy",
+    "ędliście/-łyście",
+    "ędli/-ły",
+]
 
 
 def przasc(word):
     stem = word[:-3]
+    print("Present tense:\n")
     for pronoun, suffix in enumerate(bede_forms):
+        conjugated_form = stem + suffix
+        print(pronouns[pronoun] + " " + conjugated_form)
+    print("\nPast tense (masc/fem/neut):\n")
+    for pronoun, suffix in enumerate(przasc_past_suffix):
         conjugated_form = stem + suffix
         print(pronouns[pronoun] + " " + conjugated_form)
 
