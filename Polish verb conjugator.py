9bd31b792d93b97bfd471bbfb7cad6460638e9c7
Chris Borowski <krzysztofeborowski@gmail.com>
2023-12-27 22:44:41 -0600

    Add support for future-tense forms of the -mrzeć verbs

--- Polish verb conjugator.py
+++ Polish verb conjugator.py
@@ -869,17 +869,37 @@ umrzec_past_suffix = [
 
 
 def umrzec(word):
-    print("Present tense:\n")
-    for pronoun, suffix in enumerate(umrzec_suffix):
-        stem = word[:-3]
-        conjugated_form = stem + suffix
-        print(pronouns[pronoun] + " " + conjugated_form)
-    print("\nPast tense (masc/fem/neut):\n")
-    for pronoun, suffix in enumerate(umrzec_past_suffix):
-        stem = word[:-4]
-        conjugated_form = stem + suffix
-        print(pronouns[pronoun] + " " + conjugated_form)
+    is_perfective = False
+    for prefix in perfective_prefixes:
+        if word.startswith(prefix):
+            is_perfective = True
+            break
 
+    if is_perfective:
+        print("\nPast tense (masc/fem/neut):\n")
+        for pronoun, suffix in enumerate(umrzec_past_suffix):
+            stem = word[:-4]
+            conjugated_form = stem + suffix
+            print(pronouns[pronoun] + " " + conjugated_form)
+        print("\nFuture tense:\n")
+        for pronoun, suffix in enumerate(umrzec_suffix):
+            stem = word[:-3]
+            conjugated_form = stem + suffix
+            print(pronouns[pronoun] + " " + conjugated_form)
+    else:
+        print("\nPast tense (masc/fem/neut):\n")
+        for pronoun, suffix in enumerate(umrzec_past_suffix):
+            stem = word[:-4]
+            conjugated_form = stem + suffix
+            print(pronouns[pronoun] + " " + conjugated_form)
+        print("\nPresent tense:\n")
+        for pronoun, suffix in enumerate(umrzec_suffix):
+            stem = word[:-3]
+            conjugated_form = stem + suffix
+            print(pronouns[pronoun] + " " + conjugated_form)
+        print("\nFuture tense:\n")
+        for pronoun, auxiliary in enumerate(auxiliaries):
+            print(pronouns[pronoun] + " " + auxiliary + " " + word)
 
 # wstać exception
 wstac_suffix = ["nę", "niesz", "nie", "niemy", "niecie", "ną"]
