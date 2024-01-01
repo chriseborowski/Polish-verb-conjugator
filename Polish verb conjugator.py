a13af9faa50ba7fdc9d390b25888791257918b76
Chris Borowski <krzysztofeborowski@gmail.com>
2024-01-01 17:45:55 -0600

    Add support for future-tense forms of the -wiedzieć verbs

--- Polish verb conjugator.py
+++ Polish verb conjugator.py
@@ -1498,21 +1498,43 @@ def krasc(word):
         for pronoun, auxiliary in enumerate(auxiliaries):
             print(pronouns[pronoun] + " " + auxiliary + " " + word)
 
+
 # wiedzieć exception
 wiedziec_suffix = ["m", "sz", "", "my", "cie", "dzą"]
 
 
 def wiedziec(word):
-    print("Present tense:\n")
-    for pronoun, suffix in enumerate(wiedziec_suffix):
-        stem = word[:-5]
-        conjugated_form = stem + suffix
-        print(pronouns[pronoun] + " " + conjugated_form)
-    print("\nPast tense (masc/fem/neut):\n")
-    for pronoun, suffix in enumerate(chciec_past_suffix):
-        stem = word[:-2]
-        conjugated_form = stem + suffix
-        print(pronouns[pronoun] + " " + conjugated_form)
+    is_perfective = False
+    for prefix in perfective_prefixes:
+        if word.startswith(prefix):
+            is_perfective = True
+            break
+
+    if is_perfective:
+        print("\nPast tense (masc/fem/neut):\n")
+        for pronoun, suffix in enumerate(chciec_past_suffix):
+            stem = word[:-2]
+            conjugated_form = stem + suffix
+            print(pronouns[pronoun] + " " + conjugated_form)
+        print("\nFuture tense:\n")
+        for pronoun, suffix in enumerate(wiedziec_suffix):
+            stem = word[:-5]
+            conjugated_form = stem + suffix
+            print(pronouns[pronoun] + " " + conjugated_form)
+    else:
+        print("\nPast tense (masc/fem/neut):\n")
+        for pronoun, suffix in enumerate(chciec_past_suffix):
+            stem = word[:-2]
+            conjugated_form = stem + suffix
+            print(pronouns[pronoun] + " " + conjugated_form)
+        print("\nPresent tense:\n")
+        for pronoun, suffix in enumerate(wiedziec_suffix):
+            stem = word[:-5]
+            conjugated_form = stem + suffix
+            print(pronouns[pronoun] + " " + conjugated_form)
+        print("\nFuture tense:\n")
+        for pronoun, auxiliary in enumerate(auxiliaries):
+            print(pronouns[pronoun] + " " + auxiliary + " " + word)
 
 
 # nieść category
