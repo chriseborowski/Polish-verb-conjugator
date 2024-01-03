3cc9e75369574f6af7706541718c3ca3e66c4864
Chris Borowski <krzysztofeborowski@gmail.com>
2024-01-03 13:00:23 -0600

    Add support for future-tense forms of the -tworzyć verbs

--- Polish verb conjugator.py
+++ Polish verb conjugator.py
@@ -2210,16 +2210,37 @@ tworzyc_suffix = ["ę", "ysz", "y", "ymy", "ycie", "ą"]
 
 
 def tworzyc(word):
-    print("Present tense:\n")
-    for pronoun, suffix in enumerate(tworzyc_suffix):
-        stem = word[:-2]
-        conjugated_form = stem + suffix
-        print(pronouns[pronoun] + " " + conjugated_form)
-    print("\nPast tense (masc/fem/neut):\n")
-    for pronoun, suffix in enumerate(robic_past_suffix):
-        stem = word[:-1]
-        conjugated_form = stem + suffix
-        print(pronouns[pronoun] + " " + conjugated_form)
+    is_perfective = False
+    for prefix in perfective_prefixes:
+        if word.startswith(prefix):
+            is_perfective = True
+            break
+
+    if is_perfective:
+        print("\nPast tense (masc/fem/neut):\n")
+        for pronoun, suffix in enumerate(robic_past_suffix):
+            stem = word[:-1]
+            conjugated_form = stem + suffix
+            print(pronouns[pronoun] + " " + conjugated_form)
+        print("\nFuture tense:\n")
+        for pronoun, suffix in enumerate(tworzyc_suffix):
+            stem = word[:-2]
+            conjugated_form = stem + suffix
+            print(pronouns[pronoun] + " " + conjugated_form)
+    else:
+        print("\nPast tense (masc/fem/neut):\n")
+        for pronoun, suffix in enumerate(robic_past_suffix):
+            stem = word[:-1]
+            conjugated_form = stem + suffix
+            print(pronouns[pronoun] + " " + conjugated_form)
+        print("\nPresent tense:\n")
+        for pronoun, suffix in enumerate(tworzyc_suffix):
+            stem = word[:-2]
+            conjugated_form = stem + suffix
+            print(pronouns[pronoun] + " " + conjugated_form)
+        print("\nFuture tense:\n")
+        for pronoun, auxiliary in enumerate(auxiliaries):
+            print(pronouns[pronoun] + " " + auxiliary + " " + word)
 
 
 # zapomnieć category
