d1ba2cfb905ca2706018f75fdbd50c917fc43f2d
Chris Borowski <krzysztofeborowski@gmail.com>
2024-01-06 22:06:08 -0600

    Add support for past-, present-, and future-tense forms of the -mdleć verbs

--- Polish verb conjugator.py
+++ Polish verb conjugator.py
@@ -824,6 +824,40 @@ def pic(word):
             print(pronouns[pronoun] + " " + auxiliary + " " + word)
 
 
+def mdlec(word):
+    is_perfective = False
+    for prefix in perfective_prefixes:
+        if word.startswith(prefix):
+            is_perfective = True
+            break
+
+    if is_perfective:
+        print("\nPast tense (masc/fem/neut):\n")
+        for pronoun, suffix in enumerate(chciec_past_suffix):
+            stem = word[:-2]
+            conjugated_form = stem + suffix
+            print(pronouns[pronoun] + " " + conjugated_form)
+        print("\nFuture tense:\n")
+        for pronoun, suffix in enumerate(pic_suffix):
+            stem = word[:-1]
+            conjugated_form = stem + suffix
+            print(pronouns[pronoun] + " " + conjugated_form)
+    else:
+        print("\nPast tense (masc/fem/neut):\n")
+        for pronoun, suffix in enumerate(chciec_past_suffix):
+            stem = word[:-2]
+            conjugated_form = stem + suffix
+            print(pronouns[pronoun] + " " + conjugated_form)
+        print("\nPresent tense:\n")
+        for pronoun, suffix in enumerate(pic_suffix):
+            stem = word[:-1]
+            conjugated_form = stem + suffix
+            print(pronouns[pronoun] + " " + conjugated_form)
+        print("\nFuture tense:\n")
+        for pronoun, auxiliary in enumerate(auxiliaries):
+            print(pronouns[pronoun] + " " + auxiliary + " " + word)
+
+
 # prząść exception
 przasc_suffix = ["ędę", "ędziesz", "ędzie", "ędziemy", "ędziecie", "ędą"]
 przasc_past_suffix = [
